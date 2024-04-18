2bc8c2b9808f791b9c1d00ababbfcfc8b88c963f
sugarkwork <sugarkwork@sugar-knight.com>
2024-04-18 12:08:25 +0900

    update

--- nodes.py
+++ nodes.py
@@ -1,31 +1,32 @@
-
 class TagFilter:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
-        return {            
-            "required": {                
-                "tags": ("STRING",),                
-                },            
-                }
+        return {
+            "required": {
+                "tags": ("STRING", {"default": ""}),
+            },
+        }
 
-    RETURN_TYPES = ("STRING")
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("result",)
+
     FUNCTION = "tag"
+
     CATEGORY = "text"
+
     OUTPUT_NODE = True
 
     def tag(self, tags):
         tags = [tag.strip() for tag in tags.split(",")]
         tags2 = [tag.replace("_", " ") for tag in tags]
-
         result = []
         for i, tag2 in enumerate(tags2):
             if tag2.endswith("by self"):
                 result.append(tags[i])
-
-        return {"result": (result,)}
+        return (", ".join(result),)
 
 NODE_CLASS_MAPPINGS = {
     "TagFilter": TagFilter
